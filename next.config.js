8598d20b76fa3c2b7df7dc10144a49ec31dcb5d9
Andrew Chander <13403032+taghead@users.noreply.github.com>
2021-12-03 23:26:37 +1100

    Removed bad exportPathMaps objects

--- next.config.js
+++ next.config.js
@@ -10,10 +10,10 @@ module.exports = {
   ) {
     return {
       "/": { page: "/" },
-      "/about": { page: "/about" },
-      "/p/hello-nextjs": { page: "/post", query: { title: "hello-nextjs" } },
-      "/p/learn-nextjs": { page: "/post", query: { title: "learn-nextjs" } },
-      "/p/deploy-nextjs": { page: "/post", query: { title: "deploy-nextjs" } },
+      // "/about": { page: "/about" },
+      // "/p/hello-nextjs": { page: "/post", query: { title: "hello-nextjs" } },
+      // "/p/learn-nextjs": { page: "/post", query: { title: "learn-nextjs" } },
+      // "/p/deploy-nextjs": { page: "/post", query: { title: "deploy-nextjs" } },
     };
   },
 };
