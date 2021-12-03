c1d8cef4af14a27ec7fa4c93423af0f492f0deff
Andrew Chander <13403032+taghead@users.noreply.github.com>
2021-12-03 23:21:59 +1100

    Added next js export path map

--- next.config.js
+++ next.config.js
@@ -4,4 +4,16 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   assetPrefix: isProd ? "/Royal-Kingdom-Vassals.github.io/" : "",
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      "/p/hello-nextjs": { page: "/post", query: { title: "hello-nextjs" } },
+      "/p/learn-nextjs": { page: "/post", query: { title: "learn-nextjs" } },
+      "/p/deploy-nextjs": { page: "/post", query: { title: "deploy-nextjs" } },
+    };
+  },
 };
