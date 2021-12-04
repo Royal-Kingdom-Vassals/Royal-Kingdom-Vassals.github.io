40821026e054388b18fff93d313d7e1191accc23
Seido <maurice_98@hotmail.com>
2021-12-04 23:30:24 +1100

    About Page - Initial Setup

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ) {
     return {
       "/": { page: "/" },
-      // "/about": { page: "/about" },
+      "/about": { page: "/about" },
       // "/p/hello-nextjs": { page: "/post", query: { title: "hello-nextjs" } },
       // "/p/learn-nextjs": { page: "/post", query: { title: "learn-nextjs" } },
       // "/p/deploy-nextjs": { page: "/post", query: { title: "deploy-nextjs" } },
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
+import { text } from "node:stream/consumers";
 import "tailwindcss/tailwind.css";
 
 const Home: NextPage = () => {
   return (
     <div className="flex h-screen place-content-center">
-      <div className="bg-red-400 p-7 m-auto">Hello world</div>
+      <div className="bg-red-400 p-7 m-auto">About</div>
+      <div className="bg-red-400 p-7 m-auto">About</div>
     </div>
   );
 };
